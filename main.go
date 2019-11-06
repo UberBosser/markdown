640d692f0ec225847001f57116afda9d74345753
BuildTools <uberbosser@gmail.com>
2019-11-06 12:33:05 -0500

    Remove external template

--- main.go
+++ main.go
@@ -9,13 +9,31 @@ import (
 	"os"
 )
 
+var mainTemplate = template.Must(template.New("main").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MdServer</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.css" />
+</head>
+<body class="markdown-body">
+	{{ . }}
+	<script
+		src="https://code.jquery.com/jquery-3.3.1.min.js"
+		integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+		crossorigin="anonymous">
+	</script>
+	<script>function update(){$.ajax({type:"GET",url:"/data",success:function(t){$("body").html(t.html);if(document.title!=t.name){document.title=t.name;}}})}$(document).ready(function(){update(),setInterval(update,1e3)});</script>
+</body>
+</html>`))
+
 type MarkdownFile struct {
 	Name string        `json:"name"`
 	Html template.HTML `json:"html"`
 }
 
 func render(c *gin.Context) {
-	c.HTML(200, "main.tmpl", nil)
+	c.HTML(200, "main", nil)
 }
 
 func data(c *gin.Context) {
@@ -35,13 +53,13 @@ func main() {
 	}
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
-	router.LoadHTMLGlob("./templates/main.tmpl")
+	router.SetHTMLTemplate(mainTemplate)
 
 	router.GET("/", render)
 	router.GET("/data", data)
 
 	router.NoRoute(render)
 
-	fmt.Println("Serving port :8080")
-	router.Run()
+	fmt.Println("Serving " + os.Args[1] + " to port :8080")
+	router.Run(":8080")
 }
